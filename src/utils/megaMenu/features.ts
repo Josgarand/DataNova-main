41dbe3b6ae9b3823dd4aabb5b589ae9befde88e5
Josgarand <jocefuo@gmail.com>
2025-05-03 14:36:45 +0200

    first commit

---
+++ src/utils/megaMenu/features.ts
@@ -0,0 +1,30 @@
+export const featuresMenu = [
+  {
+    sectionTitle: "AI-Powered Insights",
+    items: [
+      {
+        icon: "home",
+        title: "House 1",
+        description: "Ubicacion de la casa 1.",
+        href: "./houseInfo1",
+      },
+      {
+        icon: "home",
+        title: "House 2",
+        description: "Ubicacion de la casa 2",
+        href: "./houseInfo2",
+      },
+    ],
+  },
+  {
+    sectionTitle: "Advanced Data Tools",
+    items: [
+      {
+        icon: "home",
+        title: "House 3",
+        description: "Ubicacion de la casa 3",
+        href: "./houseInfo3",
+      },
+    ],
+  },
+];
---
+++ src/utils/navigation.ts
@@ -0,0 +1,6 @@
+export const navigationLinks = [
+  { href: "/housesBooking", label: "Reservas" },
+  { href: "/contact", label: "Contacto" },
+  // { href: "/about", label: "About" },
+  // { href: "/contact", label: "Contact" },
+];
