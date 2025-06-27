79f35912e78062386b3e9c828642ea25f45c4be5
Josgarand <jocefuo@gmail.com>
2025-06-27 19:34:02 +0200

    swiper and google fonts

---
+++ public/swiper-init.js
@@ -0,0 +1,16 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".mySwiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
